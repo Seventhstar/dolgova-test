69b4346979caa7e6e6da470ea6bc3c95b8549af6
Evgeniy Kovyryashkin <seventhstar@mail.ru>
2020-08-16 07:32:03 +0300

    first commit

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -8,6 +8,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import App from '../app.vue'
 // import VueRouter from 'vue-router'
 import router from "./router";
