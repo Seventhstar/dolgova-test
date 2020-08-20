5552ce1f8a82b97af110a001ef7fef2615c64ee7
Evgeniy Kovyryashkin <seventhstar@mail.ru>
2020-08-20 10:28:56 +0300

    first commit

--- app/javascript/packs/components/TopMenu.vue
+++ app/javascript/packs/components/TopMenu.vue
@@ -1,82 +1,82 @@
-<!--<template>-->
-<!--  <div>-->
-<!--    <v-app-bar app dark color="pink lighten-4">-->
+<template>
+  <div>
+    <v-app-bar app dark color="pink lighten-4">
 
-<!--      <div class="header">-->
-<!--        <div class="h1">{{ appTitle }}</div>-->
-<!--        <div class="h2"> {{ appSubTitle }}</div>-->
-<!--      </div>-->
+      <div class="header">
+        <div class="h1">{{ appTitle }}</div>
+        <div class="h2"> {{ appSubTitle }}</div>
+      </div>
 
 
-<!--      <v-spacer></v-spacer>-->
-<!--      <v-icon></v-icon>-->
-<!--      <v-toolbar-items class="hidden-xs-only">-->
-<!--        <v-btn text replace-->
-<!--               v-for="item in menuItems"-->
-<!--               :key="item.title"-->
-<!--               :to="item.path">-->
-<!--          {{ item.title }}-->
-<!--        </v-btn>-->
+      <v-spacer></v-spacer>
+      <v-icon></v-icon>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn text replace
+               v-for="item in menuItems"
+               :key="item.title"
+               :to="item.path">
+          {{ item.title }}
+        </v-btn>
 
-<!--      </v-toolbar-items>-->
-<!--      <v-btn icon>-->
-<!--        <v-icon>login</v-icon>-->
-<!--      </v-btn>-->
+      </v-toolbar-items>
+      <v-btn icon>
+        <v-icon>login</v-icon>
+      </v-btn>
 
-<!--&lt;!&ndash;            <v-spacer></v-spacer>&ndash;&gt;-->
-<!--&lt;!&ndash;            <v-toolbar-items class="hidden-xs-only">&ndash;&gt;-->
-<!--&lt;!&ndash;              <v-btn&ndash;&gt;-->
-<!--&lt;!&ndash;                      text&ndash;&gt;-->
-<!--&lt;!&ndash;                      v-for="item in rightMenus"&ndash;&gt;-->
-<!--&lt;!&ndash;                      :key="item.title"&ndash;&gt;-->
-<!--&lt;!&ndash;                      :href="item.path">&ndash;&gt;-->
-<!--&lt;!&ndash;                <v-icon left dark>{{ item.icon }}</v-icon>&ndash;&gt;-->
-<!--&lt;!&ndash;                {{ item.title }}&ndash;&gt;-->
-<!--&lt;!&ndash;              </v-btn>&ndash;&gt;-->
-<!--&lt;!&ndash;            </v-toolbar-items>&ndash;&gt;-->
+<!--            <v-spacer></v-spacer>-->
+<!--            <v-toolbar-items class="hidden-xs-only">-->
+<!--              <v-btn-->
+<!--                      text-->
+<!--                      v-for="item in rightMenus"-->
+<!--                      :key="item.title"-->
+<!--                      :href="item.path">-->
+<!--                <v-icon left dark>{{ item.icon }}</v-icon>-->
+<!--                {{ item.title }}-->
+<!--              </v-btn>-->
+<!--            </v-toolbar-items>-->
 
-<!--    </v-app-bar>-->
-<!--  </div>-->
-<!--</template>-->
+    </v-app-bar>
+  </div>
+</template>
 
-<!--<script>-->
-<!--  export default {-->
-<!--    name: "TopMenu",-->
-<!--    data() {-->
-<!--      return {-->
-<!--        appTitle: 'Анастасия Долгова',-->
-<!--        appSubTitle: 'Психолог',-->
-<!--        sidebar: false,-->
-<!--        rightMenus: [-->
-<!--          {title: 'Sign Up', path: '/signup', icon: 'face'},-->
-<!--          {title: 'Sign In', path: '/signin', icon: 'lock_open'}-->
-<!--        ],-->
-<!--        menuItems: [-->
-<!--          {title: 'Обо мне', path: '/'},-->
-<!--          {title: 'Блог', path: '/posts'},-->
-<!--          {title: 'Контакты', path: '/statements'},-->
-<!--        ],-->
-<!--        fab: false,-->
-<!--        hidden: false,-->
-<!--        tabs: null,-->
-<!--      }-->
-<!--    },-->
-<!--    computed: {-->
-<!--      activeFab() {-->
-<!--        switch (this.tabs) {-->
-<!--          case 'one':-->
-<!--            return {color: 'success', icon: 'share'}-->
-<!--          case 'two':-->
-<!--            return {color: 'red', icon: 'edit'}-->
-<!--          case 'three':-->
-<!--            return {color: 'green', icon: 'keyboard_arrow_up'}-->
-<!--          default:-->
-<!--            return {}-->
-<!--        }-->
-<!--      },-->
-<!--    }-->
-<!--  };-->
-<!--</script>-->
-<!--<style>-->
+<script>
+  export default {
+    name: "TopMenu",
+    data() {
+      return {
+        appTitle: 'Анастасия Долгова',
+        appSubTitle: 'Психолог',
+        sidebar: false,
+        rightMenus: [
+          {title: 'Sign Up', path: '/signup', icon: 'face'},
+          {title: 'Sign In', path: '/signin', icon: 'lock_open'}
+        ],
+        menuItems: [
+          {title: 'Обо мне', path: '/'},
+          {title: 'Блог', path: '/posts'},
+          {title: 'Контакты', path: '/statements'},
+        ],
+        fab: false,
+        hidden: false,
+        tabs: null,
+      }
+    },
+    computed: {
+      activeFab() {
+        switch (this.tabs) {
+          case 'one':
+            return {color: 'success', icon: 'share'}
+          case 'two':
+            return {color: 'red', icon: 'edit'}
+          case 'three':
+            return {color: 'green', icon: 'keyboard_arrow_up'}
+          default:
+            return {}
+        }
+      },
+    }
+  };
+</script>
+<style>
 
-<!--</style>-->
+</style>
